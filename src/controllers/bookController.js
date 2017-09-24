71c6dff4b147e059dd20267f99b8b1678c037328
mahmne <mahmod_mne@hotmail.com>
2017-09-24 16:49:49 +0300

    add bookController and homeController and index.js book.js

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,3 +1,9 @@
-exports.home = (req, res) => {
-  res.render('addBook');
+exports.get = (req, res, next) => {
+  book.post.addBook((err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('addBook', {data});
+    }
+  });
 };
