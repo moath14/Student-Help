4a083c07073921b0b9a3de75dd6a5f6e454a0900
Ghadeer93 <eng.ghadeer93@gmail.com>
2017-09-24 16:12:00 +0300

    rename Database to Datebase

--- src/models/database/db_connection.js
+++ src/models/database/db_connection.js
@@ -5,14 +5,14 @@ const pg = require('pg');
 const {Pool} = require('pg');
 require('env2')('./config.env');
 var url = '';
-if (!process.env.DATABASE_URL) {
+if (!process.env.TEST_URL) {
   throw new Error('No DATABASE_URL provided');
 }
 
 if (process.env.NODE_ENV === 'test') {
   url = process.env.TEST_URL;
 } else {
-  url = process.env.DATABASE_URL;
+  url = process.env.TEST_URL;
 }
 const pool = new Pool({connectionString: url, ssl: true});
 module.exports = pool;
