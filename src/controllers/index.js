1345079911e240cb2f6f7e6c1c6d73a6b76f4ec0
Ghadeer93 <eng.ghadeer93@gmail.com>
2017-09-24 16:09:50 +0300

    sperate controllers file

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middlewares/auth.js')
-const userController = require('./usersController');
-router.get('/home', userController.home);
-router.get('/login', userController.login);
-router.get('/signup', userController.signup);
+const signupController = require('./signupController');
+
+router.get('/signup', signupController.get);
+router.post('/signup', signupController.post);
 
 
 
--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -7,8 +7,3 @@ exports.login = (req, res) => {
 
   res.render('login')
 }
-
-exports.signup = (req, res) => {
-
-  res.render('signup')
-}
