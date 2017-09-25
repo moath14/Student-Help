5814e67ff2b90e5283878aaa9d62c0e0b13f1ba2
Ghadeer93 <eng.ghadeer93@gmail.com>
2017-09-25 09:52:50 +0300

    change in loginController - add new controller in index controller - change login function - change in query

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,8 +3,21 @@ const router = express.Router();
 
 const auth = require('../middlewares/auth.js')
 const signupController = require('./signupController');
+const loginController = require('./loginController');
+const logoutController = require('./logoutController');
 
+
+// function for signupController
 router.get('/signup', signupController.get);
 router.post('/signup', signupController.post);
+
+// function for loginController
+router.get('/login',loginController.get);
+router.post('/login',loginController.post);
+
+// function for logoutController
+router.get('/logout',loginController.get);
+router.post('/logout',loginController.post);
+
 
 module.exports = router;
