f61f2ba0a309665e2470a8465babe2387c8977b2
Ghadeer93 <eng.ghadeer93@gmail.com>
2017-09-25 12:34:36 +0300

    booksModel

---
+++ src/controllers/homeController.js
@@ -0,0 +1,41 @@
+const book = require('../models/books.js');
+
+exports.get = (req, res, next) => {
+  book.showAllBooks((err, books) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('home', {books});
+    }
+  });
+};
+
+exports.post = (req, res, next) => {
+  book.searchBookByTitle((err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('home', {data});
+    }
+  });
+};
+
+exports.post = (req, res, next) => {
+  book.searchBookByAuther((err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('home', {data});
+    }
+  });
+};
+
+exports.post = (req, res, next) => {
+  book.searchBookByISBN((err, data) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('home', {data});
+    }
+  });
+};
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const auth = require('../middlewares/auth.js')
+const home = require('./homeController');
+
+const auth = require('../middlewares/auth.js');
 const signupController = require('./signupController');
 const loginController = require('./loginController');
 const logoutController = require('./logoutController');
+const bookController = require('./bookController');
+const searchController = require('./searchController');
 
+router.get('/home', home.get);
+router.post('/home', home.post);
 
-// function for signupController
 router.get('/signup', signupController.get);
 router.post('/signup', signupController.post);
+
 
 // function for loginController
 router.get('/login',loginController.get);
@@ -18,6 +24,14 @@ router.post('/login',loginController.post);
 // function for logoutController
 router.get('/logout',loginController.get);
 router.post('/logout',loginController.post);
+
+//funation for bookController
+
+router.post('/addbook', bookController.post);
+
+//funation for searchController
+router.get('/search',searchController.get);
+// router.post('/search', searchController.post);
 
 
 module.exports = router;
