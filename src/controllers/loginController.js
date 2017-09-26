297d107d52e5f1a29724100b85e4c80bc78e7ce4
mahmne <mahmod_mne@hotmail.com>
2017-09-26 15:50:16 +0300

    fix bugs

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,39 +2,37 @@
   const {compare} = require('../helper/auth');
 
   exports.get = (req, res) => {
-    res.render('login')
-  }
+    if (req.user) {
+      res.redirect('/profile');
+    } else {
+      res.render('login');
+    }
+  };
 
-  exports.post=(req,res,next) => {
-    const email = req.body.email
-    const password = req.body.password
+  exports.post = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
     // get user by email
-  userModel.getUserByemail(email,(error, userObj) => {
-    //console.log(userObj);
-      if(error){
-        return next(error)
+    userModel.getUserByemail(email, (error, userObj) => {
+    // console.log(userObj);
+      if (error) {
+        return next(error);
       }
-      if(!userObj){
-
-        return res.render('login',{errorMessage:'password or email are not correct'});
+      if (!userObj) {
+        return res.render('login', {errorMessage: 'password or email are not correct'});
       }
-
 
       // make compare "password" && hashPassword
-    compare(password,userObj.password,(err,isMatch) => {
-
-      if(err) {
-        return res.redirect('/login')
-      }
-      if(!isMatch) {
-        return res.render('login',{errorMessage:' password are not correct'})
-      }
+      compare(password, userObj.password, (err, isMatch) => {
+        if (err) {
+          return res.redirect('/login');
+        }
+        if (!isMatch) {
+          return res.render('login', {errorMessage: ' password are not correct'});
+        }
       // we must set token in Cookies
-      res.cookie('email',email)
-      return res.redirect('/home')
-
+        res.cookie('email', email);
+        return res.redirect('/home');
+      });
     });
-
-  });
-
-  }
+  };
