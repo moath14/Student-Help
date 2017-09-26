8a9dce6b6a287f61d855dce91f51718e954f267f
Ghadeer93 <eng.ghadeer93@gmail.com>
2017-09-26 15:47:08 +0300

    add new function for home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,3 +9,39 @@ exports.get = (req, res, next) => {
     }
   });
 };
+
+exports.getabout = (req,res) => {
+  res.render('about')
+}
+
+exports.getbooks = (req,res) => {
+  book.showAllBooks((err, books) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('books', {books});
+    }
+  });
+
+}
+exports.getAllbooks = (req,res) => {
+  book.showAllBooks((err, books) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('moreBook', {books});
+    }
+  });
+
+}
+
+exports.get4books = (req,res) => {
+  book.showFourBooks((err, books) => {
+    if (err) {
+      next(err);
+    } else {
+      res.render('home', {books});
+    }
+  });
+
+}
